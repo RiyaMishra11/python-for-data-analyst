faf99163a5d84384fb910acb6eb493cc8322d05a
RIYA MISHRA <itsriya1586@gmail.com>
2026-07-30 11:10:29 +0530

    Practised basic things in Python

---
+++ dayOne.py
@@ -0,0 +1,47 @@
+# MY FIRST PYTHON PROGRAM
+
+print ("Hello Riya")
+print ("Good Morning")
+print("Welcome to Python")
+print("Let's Learn")
+
+# Topic 3: Comments
+# Kabhi-kabhi hum code me notes likhte hain taaki baad me ya kisi aur ko samajhne me aasani ho.
+# Python me comment # se likhte hain.
+#EXAMPLE--
+
+print("Hello my world")
+
+print("today's date is 30 july 2026")
+
+#VARIABLE--
+#variable_name =value
+Name ="Riya"
+age=22
+city="Noida"
+
+print(Name)
+print(age)
+print(city)
+
+print("Riya\n",22,"\nNoida")
+
+college ="MSITM Degree College"
+Course ="BCA"
+print(college)
+print(Course)
+
+# Mini Challenge
+favourite_food = "dall chawal"
+favourite_color = "blue"
+favourite_subject = "python"
+favourite_job= "data analysis"
+dream_job = "ias"
+country = "india"
+
+print(favourite_food)
+print(favourite_color)
+print(favourite_subject)
+print(favourite_job)
+print(dream_job)
+print(country)
