25964ee5e660093b2410abb6c6df2ba075a89a6d
RIYA MISHRA <itsriya1586@gmail.com>
2026-08-03 20:27:03 +0530

    Data Type
    Structured data including numeric, categorical, text, and date/time fields used for data analysis, reporting, visualization, and database management.
    
    Data Type	Description
    INT   	        Stores whole numbers (e.g., 10, 250, -5).
    FLOAT	        Stores decimal values (e.g., 45.75, 99.99).
    DECIMAL 	Stores precise decimal values, commonly used for financial data.
    CHAR	        Stores fixed-length character strings.
    VARCHAR	Stores variable-length text strings.
    TEXT	       Stores large amounts of text.
    DATE	       Stores date values (YYYY-MM-DD).
    TIME	       Stores time values (HH:MM:SS).
    DATETIME       Stores both date and time values.
    BOOLEAN        Stores TRUE/FALSE values (or 1/0).

---
+++ day03 Data Type.py
@@ -0,0 +1,36 @@
+# 1️⃣ String (str)
+# Jo bhi text hota hai wo String hota hai
+# Example:.
+
+# name = "Prashant"
+# city = "Noida"
+# course ="Btech"
+# print (name)
+# print (city)
+# print (course)
+
+# 2️⃣ Integer (int)
+# Whole numbers.
+# Example:
+
+
+# 4️⃣ Boolean (bool)
+# Sirf do values hoti hain.(True , False)
+# example.
+#is_student = True
+#is_employee = False
+#print(is_student)
+#print(is_employee) 
+
+
+
+# type() Function type ()
+# Example: 01
+name = "shriya"
+print(type(name))
+
+# example:02
+age = 20
+print(type(age))
+
+
