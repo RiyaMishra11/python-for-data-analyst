b072df58480620c285bbee51febf6ecc3d0a3009
RIYA MISHRA <itsriya1586@gmail.com>
2026-08-07 11:59:52 +0530

    Practic

---
+++ day05 Practic.py
@@ -0,0 +1,30 @@
+#Practice 1
+name = input("Enter your name")
+print("Welcome" , name)
+
+#Practice 2
+age = int(input("Enter your age: "))
+print("your age is" ,age)
+
+#Practice 3
+salary = float(input("Enter your salary"))
+print ("your salaryis", salary)
+
+#Practice 4
+city = input("Enter your city: ")
+print("You live in", city)
+
+#practice 5
+print("===== Student Information =====")
+
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+course = input("Enter Your Course: ")
+percentage = float(input("Enter Your Percentage: "))
+
+print("\n----- Student Details -----")
+
+print("Name :", name)
+print("Age :", age)
+print("Course :", course)
+print("Percentage :", percentage)
